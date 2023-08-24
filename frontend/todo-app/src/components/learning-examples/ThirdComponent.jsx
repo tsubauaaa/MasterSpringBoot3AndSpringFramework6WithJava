a465b1b4dfd2a9c1bb777b188ddf5959671e4152
tsubauaaa <tsubasa1173@gmail.com>
2023-08-24 17:58:23 +0900

    first react app done

---
+++ frontend/todo-app/src/components/learning-examples/ThirdComponent.jsx
@@ -0,0 +1,10 @@
+import { Component } from 'react'
+
+
+export default class ThirdComponent extends Component {
+    render() {
+        return (
+            <div className="ThirdComponent">Third Component</div>
+        )
+    }
+}
