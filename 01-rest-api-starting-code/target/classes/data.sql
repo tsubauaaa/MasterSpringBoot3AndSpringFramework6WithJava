cecbe682ff1bcba7497e891a935b6e9105c047ae
tsubauaaa <tsubasa1173@gmail.com>
2023-08-30 17:06:22 +0900

    connecting spring boot to react app 2 done

---
+++ 01-rest-api-starting-code/target/classes/data.sql
@@ -0,0 +1,5 @@
+insert into todo (id, description, done, target_date, username) values (10001, 'Learn AWS', false, CURRENT_DATE(), 'in28minutes')
+
+insert into todo (id, description, done, target_date, username) values (10002, 'Learn AWS Certified', false, CURRENT_DATE(), 'in28minutes')
+
+insert into todo (id, description, done, target_date, username) values (10003, 'Learn DevOps', false, CURRENT_DATE(), 'in28minutes')
